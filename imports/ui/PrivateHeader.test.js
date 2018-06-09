ab69df08dc61251e86dddc5e771b9fe57dcc2361
Mohamed Hassan <mohamed.hassan.pet@gmail.com>
2018-06-09 05:13:40 +0200

    added privatHeader test file

--- imports/ui/PrivateHeader.test.js
+++ imports/ui/PrivateHeader.test.js
@@ -3,12 +3,12 @@ import { Meteor } from 'meteor/meteor';
 import expect from 'expect';
 import { mount } from 'enzyme';
 
-import PrivateHeader from './PrivateHeader';
+import {PrivateHeader} from './PrivateHeader';
 
 if (Meteor.isClient) {
     describe('PrivateHeader',function () {
         const title = "MY Title"
-        const wrapper = mount( <PrivateHeader title={title} />);
+        const wrapper = mount( <PrivateHeader title={title} handleLogOut={ ()=> 1 } />);
         it('Button text should be "Logout" ',function(){
             const button = wrapper.find('.button').text();
             expect(button).toBe('Logout')
@@ -16,6 +16,12 @@ if (Meteor.isClient) {
         it('Button text should be "Logout" ',function(){
             const realTitle = wrapper.find('h1').text();
             expect(realTitle).toBe(title);
+        })
+        it("should call the spy", function(){
+            const spy = expect.createSpy();
+            const wrapper = mount(<PrivateHeader title={title} handleLogOut={spy} />)
+            wrapper.find('button').simulate('click')
+            expect(spy).toHaveBeenCalled()
         })
     })
 }
